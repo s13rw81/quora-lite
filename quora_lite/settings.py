b8e25c869f1e450c6bcf02af525307105d8a0571
Sourav B. Roy <wildr.slimshady@gmail.com>
2021-07-05 11:52:42 +0530

    fix django.core.exceptions.ImproperlyConfigured exception

--- quora_lite/settings.py
+++ quora_lite/settings.py
@@ -162,5 +162,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
+STATIC_URL = "/static/"
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
